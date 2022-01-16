064e6cec989506acf6e086b247bd589014e4403c
Tyler Beck <tylerbeck@Tylers-Air.attlocal.net>
2022-01-15 19:29:45 -0800

    debugged HTML added hide function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,59 @@
-function startQuiz(){
+const startButton = document.getElementById("start-btn");
+const nextButton = document.getElementById("next-btn");
+const questionContainerElement = document.getElementById("question-container");
+const questionElement = document.getElementById("question");
+const answerButtonsElement = document.getElementById("answer-btn")
 
+let shuffledQuestions, currentQuestionIndex
+
+startButton.addEventListener("click", startQuiz);
+
+function startQuiz(){
+    console.log("started");
+    startButton.classList.add("hide");
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
+    questionContainerElement.classList.remove("hide");
+    setNextQuestion()
 }
 
-function nextQuestion(){
+function setNextQuestion(){
+    resetState()
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+}
 
+function showQuestion(question){
+    questionElement.innerText = question.question
+    question.answers.forEach(answer => {
+        const button = document.createElement("button")
+        button.innerText = answer.text
+        button.classList.add("btn")
+        if (answer.correct){
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener("click", selectAnswer)
+        answerButtonsElement.appendChild(button)
+    })
+}
+
+function resetState() {
+    nextButton.classList.add("hide")
+    while (answerButtonsElement.firstChild){
+        answerButtonsElement.removeChild
+        (answerButtonsElement.firstChild)
+    }
 }
 
 function selectAnswer(){
-    
+
 }
+
+const questions = [
+    {
+        question: "What is an Array?",
+        answers: [
+            {text: "A group of values stored in a paticular order", correct: true},
+            {text: "A light beam that comes from the sky.", correct: false}
+        ]
+    }
+]

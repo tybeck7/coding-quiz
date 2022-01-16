a672144db5ec38a68cf967423a9f7125561bc91b
Tyler Beck <tylerbeck@Tylers-Air.attlocal.net>
2022-01-15 20:27:55 -0800

    added questions and next button function

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,10 @@ const answerButtonsElement = document.getElementById("answer-btn")
 let shuffledQuestions, currentQuestionIndex
 
 startButton.addEventListener("click", startQuiz);
+nextButton.addEventListener("click", () => {
+   currentQuestionIndex++
+   setNextQuestion() 
+})
 
 function startQuiz(){
     console.log("started");
@@ -44,7 +48,34 @@ function resetState() {
     }
 }
 
-function selectAnswer(){
+function selectAnswer(e) {
+    const selectedButton = e.target
+    const correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsElement.children).forEach(button => {
+    setStatusClass(button, button.dataset.correct)    
+    })
+    if (shuffledQuestions.length > currentQuestionIndex +1) {
+        nextButton.classList.remove("hide")
+    } else {
+        startButton.innerText = "restart"
+        startButton.classList.remove("hide")
+    }
+}
+
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add("correct")
+    }
+    else {
+        element.classList.add("wrong")
+    }
+}
+
+function clearStatusClass(element) {
+    element.classList.remove("correct")
+    element.classList.remove("wrong")
 
 }
 
@@ -54,6 +85,24 @@ const questions = [
         answers: [
             {text: "A group of values stored in a paticular order", correct: true},
             {text: "A light beam that comes from the sky.", correct: false}
+        ]
+    },
+    {
+        question: "What is the minimum amount of hours you should spend coding per-week to succeed?",
+        answers: [
+            {text: "10", correct: false},
+            {text: "50", correct: false},
+            {text: "35", correct: true},
+            {text: "20", correct: false},           
+        ]
+    },
+    {
+        question: "What does HTML mean?",
+        answers: [
+            {text: "How To Make Lanterns", correct: false},
+            {text: "Hyper Text Mark-up Language", correct: true},
+            {text: "Horizon Text Mark-up Language", correct: false},
+            {text: "Hyper Text Mark-up Laptop", correct: false},
         ]
     }
 ]
